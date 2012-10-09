5e5564c8048ab0fba4d6b0b18554a849807bbfa4
PeterTheOne <petertheone@gmail.com>
2012-10-09 23:34:58 +0200

    add url and apiKey grabber

--- js/application.js
+++ js/application.js
@@ -1,3 +1,14 @@
 $(function(){
     lqfbDelegationGraph.start();
 });
+
+function getURLParameter(name) {
+    var result = decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
+    if (result && result != 'null') {
+        return result;
+    }
+    return null;
+}
+
--- js/router.js
+++ js/router.js
@@ -4,11 +4,20 @@ window.lqfbDelegationGraph = new (Backbone.Router.extend({
     },
 
     initialize: function() {
+        this.baseUrl = 'http://apitest.liquidfeedback.org:25520';
+        this.apiKey = null;
 
+        var parameterUrl = getURLParameter('baseUrl');
+        var apiKey = getURLParameter('apiKey');
+        if (parameterUrl && apiKey) {
+            this.baseUrl = parameterUrl;
+            this.apiKey = apiKey;
+        }
     },
 
     index: function() {
         this.memberList = new MemberList();
+        this.memberList.url = this.baseUrl + '/member';
         this.memberListView = new MemberListView({collection: this.memberList});
         this.memberListView.render();
         $('#canvas').html(this.memberListView.el);
